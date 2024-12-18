db6c30bd2ed73718f6199fc849223d2ab4c0c6b0
kyroluskamal <kyrolussous@gmail.com>
2024-12-18 10:55:00 +0100

    [38] Reactive forms - شرح Dynamic Validation في  #angular18 + [ #انجولار+18 ]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { CustomValidators } from '../CustomValidation/customvalidtors';
+
+//clearValidators
+//removeValidators
+//removeAsyncValidators
+
+//setValidators
+//addValidators
+//setAsyncValidators
+//addAsyncValidators
+//updateValueAndValidity
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,15 +19,25 @@ import { CustomValidators } from '../CustomValidation/customvalidtors';
 })
 export class AppComponent {
   title = 'Coding Bible library';
+  valdiatoMinleng = Validators.minLength(5);
   fb = inject(FormBuilder);
-  form = this.fb.group(
-    {
-      password: ['', [Validators.required]],
-      confirmPassword: ['', [Validators.required]],
-    },
-    { validators: CustomValidators.passwordMatch2() }
-  );
-
+  form = this.fb.group({
+    address: ['', [Validators.required]],
+    skills: ['dd', [Validators.required, this.valdiatoMinleng]],
+    addAddressAndSkills: [true],
+  });
+  ngOnInit() {
+    this.form.controls.addAddressAndSkills.valueChanges.subscribe((checked) => {
+      if (!checked) {
+        this.form.controls.address.clearValidators();
+        this.form.controls.skills.removeValidators(this.valdiatoMinleng);
+      } else {
+        this.form.controls.address.setValidators([Validators.required]);
+        this.form.controls.skills.addValidators([this.valdiatoMinleng]);
+      }
+      this.form.updateValueAndValidity();
+    });
+  }
   submit() {
     if (this.form.invalid) return;
     console.log(this.form.value);
