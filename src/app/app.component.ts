8a22a0ac1d0a0f70726c374d4efcd871cf5fc2ca
kyroluskamal <kyrolussous@gmail.com>
2025-02-09 13:29:04 +0100

    [041] استخدام FormRecord في Angular وفهم Typed وUntyped Forms والفرق بينهم [#angular18+|#انجولار18+]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,26 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-
-/************************************
- * User interaction states
- *************************************/
-// pristine
-// dirty
-// touched
-// untouched
-// disabled
-// enabled
-/************************************
- * Validation states
- *************************************/
-// pending
-// valid
-// invalid
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormRecord,
+  NonNullableFormBuilder,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+interface UserFormGroup {
+  email?: (string | ValidationErrors)[];
+  name?: string[];
+  address: FormGroup<AddressFormGroup>;
+  skills: FormArray<FormControl<string>>;
+}
+interface AddressFormGroup {
+  street: FormControl<string | null>;
+  city: FormControl<string | null>;
+  state: FormControl<string | null>;
+  zip: FormControl<string | null>;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -25,29 +30,37 @@ import { FormBuilder } from '@angular/forms';
 })
 export class AppComponent {
   title = 'Coding Bible library';
+  formContol = new FormControl<string | null>(null);
   fb = inject(FormBuilder);
+
+  form2 = new FormRecord<FormControl<string | null>>({
+    name: new FormControl(''),
+  }); // empty object
   form = this.fb.group({
-    email: [''],
-    password: [''],
+    email: [2, [Validators.required]],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: [''],
+    }),
+    skills: new FormArray<FormControl<string>>([]),
   });
-  ngOnInit() {
-    this.form.controls.email.statusChanges.subscribe((status) => {
-      console.log(status);
-    });
+
+  get formKeys() {
+    return Object.keys(this.form2.controls);
   }
+  ngOnInit() {}
   submit() {
     if (this.form.invalid) return;
     console.log(this.form.value);
   }
-
-  patchVallue() {
-    this.form.patchValue({
-      email: 'email@gmail.com',
-      password: 'password',
-    });
+  addControl() {
+    const controlName = `control${this.formKeys.length + 1}`;
+    this.form2.addControl(controlName, new FormControl(''));
   }
 
-  onInput() {
-    this.form.controls.password.setValue(this.form.controls.email.value + 'a');
+  removeControl(controlName: string) {
+    this.form2.removeControl(controlName);
   }
 }
